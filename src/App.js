6910c373cb608452aa3314e76d2aa1e07c80c692
Rob Morandini <robjmorandini@hotmail.com>
2021-04-07 21:30:22 -0400

    Add basic memory card click functionality

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Card from './components/Card';
 import {Component} from 'react';
 
 class App extends Component {
@@ -7,10 +8,12 @@ class App extends Component {
     this.state = {
       highScore: 0,
       currentScore: 0,
+      cardsClicked: [],
     }
     
     this.increaseScore = this.increaseScore.bind(this);
     this.resetScore = this.resetScore.bind(this);
+    this.clickCard = this.clickCard.bind(this);
   }
 
   increaseScore() {
@@ -18,7 +21,7 @@ class App extends Component {
       this.setState({
         currentScore: this.state.currentScore + 1,
         highScore: this.state.currentScore + 1
-      })
+      });
     } else {
       this.setState({
         currentScore: this.state.currentScore + 1
@@ -28,13 +31,29 @@ class App extends Component {
 
   resetScore() {
     this.setState({
-      currentScore: 0
+      currentScore: 0,
+      cardsClicked: [],
     });
+  }
+
+  clickCard(cardID) {
+    if (this.state.cardsClicked.includes(cardID)) {
+      this.resetScore();
+    } else {
+      this.setState({
+        cardsClicked: this.state.cardsClicked.concat([cardID]),
+      });
+      this.increaseScore();
+    }
   }
 
   render() {
     return <div>
-      Hi!
+    <p>Current Score: {this.state.currentScore}</p>
+    <p>High Score: {this.state.highScore}</p>
+      {[0, 1, 2, 3, 4, 5, 6, 7].map(value => {
+        return <Card id={value} onCardClicked={() => this.clickCard(value)}/>;
+      })}
     </div>
   }
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 function Card(props) {
     return (
-        <div className='card-container'>
+        <div className='card-container' onClick={() => props.onCardClicked()}>
             <img src={props.imgSource} alt='' />
+            <p>{props.id}</p>
         </div>
     )
 }
