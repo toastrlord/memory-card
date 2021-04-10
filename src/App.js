dcd7a800c1115013e3d0760b93eff65bd21cf29c
Rob Morandini <robjmorandini@hotmail.com>
2021-04-10 09:00:58 -0400

    Add card images

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import Card from './components/Card';
 import {Component} from 'react';
 
+const images = {
+  '1': 'balmoral',
+  '2': 'bloodhawk',
+  '3': 'brigand',
+  '4': 'devastator',
+  '5': 'fury',
+  '6': 'kestrel',
+  '7': 'peacemaker'
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -51,8 +61,8 @@ class App extends Component {
     return <div>
     <p>Current Score: {this.state.currentScore}</p>
     <p>High Score: {this.state.highScore}</p>
-      {[0, 1, 2, 3, 4, 5, 6, 7].map(value => {
-        return <Card id={value} onCardClicked={() => this.clickCard(value)}/>;
+      {[1, 2, 3, 4, 5, 6, 7].map(value => {
+        return <Card id={value} onCardClicked={() => this.clickCard(value)} imgSource={'images/' + images[value] + '.png'}/>;
       })}
     </div>
   }
